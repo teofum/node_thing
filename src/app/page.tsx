df98ed3daecab6667220a170d2b47b08abc15a9d
Teo Fumagalli <teo.fum@gmail.com>
2025-08-22 16:07:15 -0300

    revert background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Canvas } from "./components/canvas";
 
 export default function Home() {
   return (
-    <div className="font-sans bg-green-700 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Canvas />
       </main>
