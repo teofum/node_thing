ce715f30253ea150152361e1ba152de749da7a19
Sebalee <153123266+SebaNLee@users.noreply.github.com>
2025-08-23 00:27:02 -0300

    feat: React Flow component works
    subí lo del manual de Quick Start de React Flow, poco más
    
    está cursed pero anda
    ni idea cómo se cambia de color por ahora

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import { Canvas } from "./components/canvas";
+import { Workspace } from "./components/workspace";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Canvas />
-
-        {/* test commit */}
-
+        <Workspace />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
