1ef7d9a66d1e7082632301a96557be75411aa2a1
Sebalee <153123266+SebaNLee@users.noreply.github.com>
2025-08-26 13:16:32 -0300

    feat: general layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,43 @@ import { Workspace } from "./components/workspace";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Canvas />
-        <Workspace />
-      </main>
+
+    // hago la estructura básica todo con divs
+
+    // div general para definir el grid (de 3 columnas)
+    <div className="font-sans grid grid-rows-[auto_auto_1fr] min-h-screen pb-20">
+
+      {/* header */}
+      <div className="flex border p-4">
+        HEADER
+      </div> 
+
+      {/* barra de herramientas */}
+      <div className="border p-4">
+        BARRA DE HERRAMIENTAS
+      </div>
+
+      {/* panel principal (otro grid, de 3 columnas) */}
+      <div className="grid grid-cols-[15%_auto_20%]">
+
+        {/* panel izquierdo (drag & drop) */}
+        <div className="border p-4">
+          PANEL IZQ
+        </div>
+
+        {/* panel central (workspace) */}
+        <div className="border p-4">
+          <Canvas />
+          <Workspace />
+          ...
+        </div>
+
+        {/* panel derecho */}
+        <div className="border p-4">
+          PANEL DER
+        </div>
+      </div>
+      
     </div>
   );
 }
