52472616b68f8ada744ebb742da6baa706f55510
Sebalee <sebalee@PCCometLakeV2>
2025-08-30 21:17:13 -0300

    chore: ShaderNode testing

---
+++ src/app/components/workspace.tsx
@@ -0,0 +1,94 @@
+"use client"; // TODO this ok?
+
+import { useState, useCallback } from "react";
+import {
+  ReactFlow,
+  addEdge,
+  applyNodeChanges,
+  applyEdgeChanges,
+  type Node,
+  type Edge,
+  type FitViewOptions,
+  type OnConnect,
+  type OnNodesChange,
+  type OnEdgesChange,
+  type OnNodeDrag,
+  type DefaultEdgeOptions,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+import { ShaderNode } from "./shaderNode";
+import { NODE_TYPES } from "@/utils/node-type";
+
+const nodeTypes = {
+  ShaderNode,
+};
+
+// TODO debug
+const testNode = { id: "testid", type: NODE_TYPES.dummy };
+
+const initialNodes: Node[] = [
+  { id: "1", data: { label: "Node 1" }, position: { x: 5, y: 5 } },
+  { id: "2", data: { label: "Node 2" }, position: { x: 5, y: 100 } },
+  {
+    // ejemplo de uso de ShaderNode
+    id: "testid",
+    type: "ShaderNode",
+    data: {
+      node: testNode,
+    },
+    position: { x: 0, y: 5 },
+  },
+];
+
+const initialEdges: Edge[] = [{ id: "e1-2", source: "1", target: "2" }];
+
+const fitViewOptions: FitViewOptions = {
+  padding: 0.2,
+};
+
+const defaultEdgeOptions: DefaultEdgeOptions = {
+  animated: true,
+};
+
+const onNodeDrag: OnNodeDrag = (_, node) => {
+  console.log("drag event", node.data);
+};
+
+export function Workspace() {
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  const onNodesChange: OnNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes],
+  );
+  const onEdgesChange: OnEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges],
+  );
+  const onConnect: OnConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges],
+  );
+
+  return (
+    // esto es el "canvas principal" para React Flow
+    // tiene un CSS modificando esto en /node_modules/@xyflow/react/dist/style.css
+
+    // TODO meto tailwind de juguete, esto puede estar mal
+    <div className="w-screen h-screen border-6">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onNodeDrag={onNodeDrag}
+        nodeTypes={nodeTypes}
+        fitView
+        fitViewOptions={fitViewOptions}
+        defaultEdgeOptions={defaultEdgeOptions}
+      />
+    </div>
+  );
+}
