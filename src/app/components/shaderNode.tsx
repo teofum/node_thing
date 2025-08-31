922296f6ec6ad9dd1189bb9b81e349a5daabed5a
Sebalee <sebalee@PCCometLakeV2>
2025-08-30 23:52:26 -0300

    feat: ShaderNode now renders Handles dynamically

--- src/app/components/shaderNode.tsx
+++ src/app/components/shaderNode.tsx
@@ -4,6 +4,7 @@
 
 import { Handle, Position } from "@xyflow/react";
 import { Edge, Node } from "@/schemas/node.schema";
+import { NODE_TYPES } from "@/utils/node-type";
 
 export type ShaderNodeProps = {
   id: string;
@@ -12,16 +13,36 @@ export type ShaderNodeProps = {
 
 export function ShaderNode({ id, data }: ShaderNodeProps) {
   const { node } = data; // TODO, acá agregaríamos los valores de runtime
+  const nodeTypeInfo = NODE_TYPES[node.type];
 
   // TODO acá habría que renderizar y mostrar menú para cada atributo y demás
-  // TODO tmb varíar la cantidad de <Handle />s
 
   return (
-    <div className="p-2">
-      {/* TODO por ahora imprimo solo el id para testear */}
-      <div className=" text-blue-400">{node.id}</div>
-      <Handle type="source" position={Position.Right} />
-      <Handle type="target" position={Position.Left} />
+    <div className="p-2 bg-amber-500">
+      <div className=" text-blue-400">{node.id}</div> {/* debug */}
+      <div className="text-xs text-gray-400 mb-2">{nodeTypeInfo.name}</div>{" "}
+      {/* debug */}
+      {/*
+       * NOTA: React Flow usa internamente Handle id y Edge sourceHandle/targetHandle para identificar handles
+       * sería luego conectarlo con nuestro Edge del schema
+       */}
+      {/* dinámicamente chequeo campos de inputs y outputs para imprimir Handles */}
+      {/* inputs */}
+      {Object.entries(nodeTypeInfo.inputs).map(([key], index) => (
+        <>
+          <Handle type="source" position={Position.Left} id={key.toString()} />
+          <div className=" text-yellow-400">s-h: {key.toString()}</div>{" "}
+          {/* debug */}
+        </>
+      ))}
+      {/* outputs */}
+      {Object.entries(nodeTypeInfo.outputs).map(([key], index) => (
+        <>
+          <Handle type="target" position={Position.Right} id={key.toString()} />
+          <div className=" text-yellow-400">t-h: {key.toString()}</div>{" "}
+          {/* debug */}
+        </>
+      ))}
     </div>
   );
 }
