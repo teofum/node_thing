6e2536897a7735fa7c1459be60ee4ba51e4955a2
Sebalee <slee@itba.edu.ar>
2025-09-05 13:53:42 -0300

    feat: sidebar toggle

--- src/app/components/workspace/index.tsx
+++ src/app/components/workspace/index.tsx
@@ -4,15 +4,18 @@ import { ReactFlowProvider } from "@xyflow/react";
 
 import { Sidebar } from "./sidebar";
 import { Viewport } from "./viewport";
+import { useState } from "react";
 
 export function Workspace() {
+  const [hideSidebar, setHideSidebar] = useState(false);
+
   return (
     <>
       <ReactFlowProvider>
         <div className="relative w-full h-full flex-1 min-h-0">
           <Viewport />
 
-          <Sidebar />
+          <Sidebar hideSidebar={hideSidebar} setHideSidebar={setHideSidebar} />
         </div>
       </ReactFlowProvider>
     </>
--- src/app/components/workspace/sidebar.tsx
+++ src/app/components/workspace/sidebar.tsx
@@ -2,24 +2,53 @@ import React from "react";
 import { NODE_TYPES } from "@/utils/node-type";
 import cn from "classnames";
 
-export function Sidebar() {
+export function Sidebar({
+  hideSidebar,
+  setHideSidebar,
+}: {
+  hideSidebar: boolean;
+  setHideSidebar: (v: boolean) => void;
+}) {
   const onDragStart = (event: React.DragEvent, nodeType: string) => {
     event.dataTransfer.effectAllowed = "move";
     event.dataTransfer.setData("type", nodeType);
   };
 
   return (
-    <aside className="absolute left-2 top-2 bottom-2 w-52 z-10 flex flex-col rounded p-2 overflow-auto bg-gradient-to-b to-gray-700/20 via-gray-600/20 from-gray-500/20 backdrop-blur-sm border border-gray-700 shadow-lg">
-      {Object.entries(NODE_TYPES).map(([key, type]) => (
-        <div
-          key={key}
-          className="p-3 m-3 border-2 bg-black rounded-md"
-          onDragStart={(event) => onDragStart(event, key)}
-          draggable
-        >
-          {type.name}
-        </div>
-      ))}
-    </aside>
+    <>
+      <aside
+        className={cn(
+          "absolute top-2 bottom-2 z-10 flex flex-col rounded-lg p-2 overflow-hidden",
+          "bg-gradient-to-b from-gray-500/20 via-gray-600/20 to-gray-700/20",
+          "backdrop-blur-sm border border-gray-700 shadow-lg transition-all duration-50",
+          hideSidebar ? "left-0 w-2" : "left-2 w-52",
+        )}
+      >
+        {!hideSidebar &&
+          Object.entries(NODE_TYPES).map(([key, type]) => (
+            <div
+              key={key}
+              className="p-3 m-3 border-2 bg-black rounded-md"
+              onDragStart={(event) => onDragStart(event, key)}
+              draggable
+            >
+              {type.name}
+            </div>
+          ))}
+      </aside>
+
+      <button
+        className={cn(
+          "absolute top-1/2 w-6 h-14 items-center justify-center",
+          "bg-gradient-to-b from-gray-500/20 via-gray-600/20 to-gray-700/20",
+          "backdrop-blur-sm border border-gray-700 border-l-0 shadow-lg rounded",
+          "hover:bg-gray-700 transition",
+          hideSidebar ? "left-[16px]" : "left-[214px]", // esto hardcodeado dependiendo del tamaño de sidebar (también hardcodeado)
+        )}
+        onClick={() => setHideSidebar(!hideSidebar)}
+      >
+        {hideSidebar ? ">" : "<"}
+      </button>
+    </>
   );
 }
