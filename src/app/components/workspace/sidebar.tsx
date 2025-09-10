a5d4e06fb1c7d7ffc1d23d2d6781860b3b667944
Summar-ice <71830907+SantiagoLifischtz@users.noreply.github.com>
2025-09-10 17:27:17 -0300

    sidebar accordion

--- src/app/components/workspace/sidebar.tsx
+++ src/app/components/workspace/sidebar.tsx
@@ -1,5 +1,7 @@
 import React, { Fragment, useLayoutEffect, useRef, useState } from "react";
 import { LuGitFork, LuPin } from "react-icons/lu";
+import { Accordion } from "radix-ui";
+import { LuChevronDown } from "react-icons/lu";
 import cn from "classnames";
 
 import { NODE_TYPES } from "@/utils/node-type";
@@ -64,23 +66,96 @@ export function Sidebar() {
           </ToggleButton>
         </div>
         <div className="border-t border-white/15 p-2 flex flex-col gap-3 min-h-0 overflow-auto">
-          {Object.entries(nodesByCategory).map(([category, types]) => (
-            <Fragment key={category}>
-              <h3 className="text-sm/4 mt-1 font-semibold">{category}</h3>
-              {Object.entries(types).map(([key, type]) => (
-                <div
-                  key={key}
-                  className="p-3 border border-white/15 bg-black/40 rounded-md cursor-grab"
-                  onDragStart={(event) => onDragStart(event, key)}
-                  draggable
-                >
-                  {type.name}
-                </div>
-              ))}
-            </Fragment>
-          ))}
+          <Accordion.Root
+            className="AccordionRoot"
+            type="single"
+            collapsible>
+            {Object.entries(nodesByCategory).map(([category, types]) => (
+              <Fragment key={category}>
+                <AccordionItem className="AccordionItem" value={category}>
+                  <AccordionTrigger>{category}</AccordionTrigger>
+                  <AccordionContent>
+                    {Object.entries(types).map(([key, type]) => (
+                      <div
+                        key={key}
+                        className="p-3 border border-white/15 bg-black/40 rounded-md cursor-grab"
+                        onDragStart={(event) => onDragStart(event, key)}
+                        draggable
+                      >
+                        {type.name}
+                      </div>
+                    ))}
+                  </AccordionContent>
+                </AccordionItem>
+              </Fragment>
+            ))}
+          </Accordion.Root>
         </div>
       </aside>
     </>
   );
 }
+
+type AccordionItemProps = React.ComponentPropsWithoutRef<typeof Accordion.Item>;
+
+const AccordionItem = React.forwardRef<
+  React.ComponentRef<typeof Accordion.Item>,
+  AccordionItemProps
+>(({ children, className, ...props }, forwardedRef) => (
+  <Accordion.Item
+    className={cn(
+      "mt-px overflow-hidden first:mt-0 first:rounded-t last:rounded-b focus-within:relative focus-within:z-10 focus-within:shadow-[0_0_0_2px] focus-within:shadow-mauve12",
+      className
+    )}
+    {...props}
+    ref={forwardedRef}
+  >
+    {children}
+  </Accordion.Item>
+));
+
+type AccordionTriggerProps = React.ComponentPropsWithoutRef<
+  typeof Accordion.Trigger
+>;
+
+const AccordionTrigger = React.forwardRef<
+  React.ComponentRef<typeof Accordion.Trigger>,
+  AccordionTriggerProps
+>(({ children, className, ...props }, forwardedRef) => (
+  <Accordion.Header className="flex">
+    <Accordion.Trigger
+      className={cn(
+        "group flex h-[45px] flex-1 cursor-default items-center justify-between bg-mauve1 px-5 text-[15px] leading-none text-violet11 shadow-[0_1px_0] shadow-mauve6 outline-none hover:bg-mauve2",
+        className
+      )}
+      {...props}
+      ref={forwardedRef}
+    >
+      {children}
+      <LuChevronDown
+        className="text-violet10 transition-transform duration-300 ease-[cubic-bezier(0.87,_0,_0.13,_1)] group-data-[state=open]:rotate-180"
+        aria-hidden
+      />
+    </Accordion.Trigger>
+  </Accordion.Header>
+));
+
+type AccordionContentProps = React.ComponentPropsWithoutRef<
+  typeof Accordion.Content
+>;
+
+const AccordionContent = React.forwardRef<
+  React.ComponentRef<typeof Accordion.Content>,
+  AccordionContentProps
+>(({ children, className, ...props }, ref) => (
+  <Accordion.Content
+    className={cn(
+      "overflow-hidden bg-mauve2 text-[15px] text-mauve11 data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown",
+      className
+    )}
+    {...props}
+    ref={ref}
+  >
+    <div className="px-2 py-[15px]">{children}</div>
+  </Accordion.Content>
+));
