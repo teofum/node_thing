9370662ddf3c7af7a2a599c94a2c7f9488e47cec
JuaniBridoux <juani@Juani>
2025-09-11 21:58:02 -0300

    Added image preview placeholder

--- src/app/components/marketplace/shadercard.tsx
+++ src/app/components/marketplace/shadercard.tsx
@@ -9,13 +9,18 @@ export default function ShaderCard({ title, price }: ShaderCardProps) {
   return (
     <div className="glass glass-border p-6 rounded-2xl">
       <h3 className="text-xl font-semibold text-white mb-4">{title}</h3>
+      <img
+        src="https://redthread.uoregon.edu/files/original/affd16fd5264cab9197da4cd1a996f820e601ee4.png"
+        alt="Shader preview"
+        className="w-full h-auto my-2 rounded-lg"
+      />
       <h3 className="text-2xl font-bold text-teal-400">${price}</h3>
       <button
         className="absolute bottom-4 right-4 flex items-center justify-center 
                    w-8 h-8 rounded-md 
                    transition-colors shadow-md"
       >
-        <LuCirclePlus className="text-white hover:text-purple-500 w-5 h-5" />
+        <LuCirclePlus className="text-white hover:text-purple-500 w-7 h-7 " />
       </button>
     </div>
   );
