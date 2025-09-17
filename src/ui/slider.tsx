38f78d420643212a28a1853329e0e8d7e52e1df3
SantiagoLifischtz <santilifischtz@gmail.com>
2025-09-17 20:53:28 -0300

    input NaN fix

--- src/ui/slider.tsx
+++ src/ui/slider.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as Slider from "@radix-ui/react-slider";
 import { Input } from "@/ui/input";
 import cn from "classnames";
@@ -25,14 +25,19 @@ export const SliderInput = React.forwardRef<HTMLInputElement, SliderInputProps>(
   ) => {
     const [internalValue, setInternalValue] = useState(value.toString());
 
+    useEffect(() => {
+      setInternalValue(value.toString());
+    }, [value]);
+
     const updateValue = () => {
-      const newVal = clamp(parseFloat(internalValue), min, max);
+      let newVal = clamp(parseFloat(internalValue), min, max);
+      if (isNaN(newVal)) newVal = 0;
       setInternalValue(newVal.toString());
-      if (!isNaN(newVal)) onChange(newVal);
+      onChange(newVal);
     };
 
     return (
-      <div className="flex h-5 gap-1">
+      <div className="flex h-5 gap-1 nodrag" ref={forwardedRef}>
         <Slider.Root
           className={cn("relative flex items-center", className)}
           min={min}
@@ -45,17 +50,16 @@ export const SliderInput = React.forwardRef<HTMLInputElement, SliderInputProps>(
           }}
           {...props}
         >
-          <Slider.Track className="relative h-[3px] grow rounded-full bg-black">
+          <Slider.Track className="relative h-[3px] grow rounded-full bg-neutral-800">
             <Slider.Range className="absolute h-full rounded-full bg-white" />
           </Slider.Track>
           <Slider.Thumb className="block size-3 rounded-[10px] bg-white shadow-[0_0_5px] shadow-white/75 hover:bg-neutral-300 focus:shadow-[0_0_0_2px] focus:shadow-white/25 focus:outline-none transition duration-100" />
         </Slider.Root>
 
         <Input
-          ref={forwardedRef}
           variant="outline"
           size="sm"
-          className="min-w-10 w-0 !text-xs nodrag"
+          className="min-w-10 w-0 !text-xs"
           value={internalValue}
           onChange={(ev) => {
             setInternalValue(ev.target.value);
