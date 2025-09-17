80b1b5cc4c02001f2d9b8e9ee4a2c4b12c2bfa63
Sebalee <slee@itba.edu.ar>
2025-09-16 23:26:37 -0300

    refactor: PR feedbacks

--- src/app/components/workspace/menu-layers.tsx
+++ src/app/components/workspace/menu-layers.tsx
@@ -10,10 +10,14 @@ import cn from "classnames";
 
 import { Button } from "@/ui/button";
 import { useStore } from "@/store/store";
+import { BsTranslate } from "react-icons/bs";
 
 export function MenuLayers() {
-  const { setActiveLayer, addLayer, layers, currentLayer, reorderLayers } =
-    useStore();
+  const setActiveLayer = useStore((s) => s.setActiveLayer);
+  const addLayer = useStore((s) => s.addLayer);
+  const layers = useStore((s) => s.layers);
+  const currentLayer = useStore((s) => s.currentLayer);
+  const reorderLayers = useStore((s) => s.reorderLayers);
 
   const addLayerButton = () => {
     addLayer();
@@ -32,30 +36,34 @@ export function MenuLayers() {
     <div className="border-t border-white/15 flex flex-col min-h-0 overflow-auto">
       <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="layers">
+          {/* TODO quite flex-col-reverse porque rompe dnd */}
           {(provided) => (
             <div ref={provided.innerRef} {...provided.droppableProps}>
               {Array.from({ length: layers.length }).map((_, idx) => (
                 <Draggable key={idx} draggableId={`${idx}`} index={idx}>
                   {(provided, snapshot) => (
+                    // TODO no supe cómo centrar la preview del dnd (sale como si uno lo agarrase de izq arriba)
                     <div
                       ref={provided.innerRef}
                       {...provided.draggableProps}
                       style={{
                         ...provided.draggableProps.style,
-                        transform: provided.draggableProps.style?.transform,
-                        position: snapshot.isDragging ? "absolute" : undefined,
+                        position: snapshot.isDragging ? "absolute" : "static",
                       }}
                     >
-                      <Button
+                      <div
                         key={idx}
-                        variant={"outline"}
                         onClick={() => setActiveLayer(idx)}
                         className={cn(
+                          "relative p-3 pl-1 gap-3 flex flex-row items-center border-b border-white/15 hover:bg-white/5",
                           "transition-colors w-full border-white/15 hover:bg-white/5",
                           idx === currentLayer && "bg-white/7",
                         )}
                       >
-                        <div {...provided.dragHandleProps} className="z-20">
+                        <div
+                          {...provided.dragHandleProps}
+                          className="z-20 flex flex-col"
+                        >
                           <LuGripVertical className="text-white/40" />
                         </div>
 
@@ -74,11 +82,12 @@ export function MenuLayers() {
                           <LuEllipsisVertical />
                           {/* TODO acá opción cambiar nombre */}
                         </Button>
-                      </Button>
+                      </div>
                     </div>
                   )}
                 </Draggable>
               ))}
+              {provided.placeholder}
             </div>
           )}
         </Droppable>
