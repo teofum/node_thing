b59323aa9aae29f3d58b62b1a340d72650019d33
Sebalee <slee@itba.edu.ar>
2025-09-17 15:25:42 -0300

    feat: add load button

--- src/app/components/menu/file.tsx
+++ src/app/components/menu/file.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { Menu, MenuItem } from "@/ui/menu-bar";
-import { LuSave } from "react-icons/lu";
+import { LuSave, LuDownload } from "react-icons/lu";
 
 export function FileMenu() {
   return (
     <Menu label="File" value="file">
       <MenuItem icon={<LuSave />}>Save</MenuItem>
+      <MenuItem icon={<LuDownload />}>Load</MenuItem>
     </Menu>
   );
 }
