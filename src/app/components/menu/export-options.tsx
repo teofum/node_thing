b1c0b13149040fa635ee08c7b87d07700c7c5244
Teo Fumagalli <teo.fum@gmail.com>
2025-09-24 18:46:22 -0300

    export options dialog

---
+++ src/app/components/menu/export-options.tsx
@@ -0,0 +1,83 @@
+import { ComponentProps, useState } from "react";
+
+import { Button } from "@/ui/button";
+import { Dialog, DialogClose } from "@/ui/dialog";
+import { useUtilityStore } from "@/store/utility.store";
+import { saveImageToFile } from "@/utils/image";
+import { imageTypeSchema } from "@/schemas/asset.schema";
+import { Select, SelectItem } from "@/ui/select";
+import { SliderInput } from "@/ui/slider";
+
+type ExportOptionsProps = {
+  open: ComponentProps<typeof Dialog>["open"];
+  onOpenChange: ComponentProps<typeof Dialog>["onOpenChange"];
+};
+
+export function ExportOptions({ open, onOpenChange }: ExportOptionsProps) {
+  const canvas = useUtilityStore((s) => s.canvas);
+  const onNextRenderFinished = useUtilityStore((s) => s.onNextRenderFinished);
+
+  const [type, setType] = useState("png");
+  const [quality, setQuality] = useState(75);
+
+  const exportImage = () => {
+    canvas?.toBlob(
+      async (blob) => {
+        if (!blob) return;
+        const data = new Uint8Array(await blob.arrayBuffer());
+        const type = imageTypeSchema.parse(
+          blob.type.split("/").at(-1) ?? "unknown",
+        );
+
+        saveImageToFile("export", { data, type });
+      },
+      `image/${type}`,
+      quality / 100,
+    );
+  };
+
+  return (
+    <Dialog
+      title="Export"
+      description="Save an image to your computer"
+      trigger={null}
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      <div className="flex-1 grid grid-cols-[auto_12rem] border-b border-white/15 gap-3 p-3 place-content-center items-center text-right">
+        <div className="text-sm/4 font-semibold">Format</div>
+        <Select value={type} onValueChange={setType}>
+          <SelectItem value="png">PNG</SelectItem>
+          <SelectItem value="jpeg">JPEG</SelectItem>
+          <SelectItem value="webp">WebP</SelectItem>
+        </Select>
+
+        {type !== "png" ? (
+          <>
+            <div className="text-sm/4 font-semibold">Quality</div>
+            <SliderInput
+              className="w-full"
+              value={quality}
+              onChange={setQuality}
+              min={0}
+              max={100}
+              step={1}
+            />
+          </>
+        ) : null}
+      </div>
+
+      <div className="p-3 flex flex-row gap-2 justify-end items-end">
+        <DialogClose asChild>
+          <Button
+            variant="outline"
+            className="col-start-1 col-span-2"
+            onClick={() => onNextRenderFinished(exportImage)}
+          >
+            Save image
+          </Button>
+        </DialogClose>
+      </div>
+    </Dialog>
+  );
+}
--- src/app/components/menu/file.tsx
+++ src/app/components/menu/file.tsx
@@ -1,18 +1,23 @@
 "use client";
 
+import { useState } from "react";
+import { LuSave, LuDownload, LuFolderOpen, LuFileImage } from "react-icons/lu";
+
 import { Menu, MenuItem, MenuSeparator } from "@/ui/menu-bar";
-import { LuSave, LuDownload, LuFolderOpen } from "react-icons/lu";
 import { useMainStore } from "@/store/main.store";
-import { saveJsonToFile, loadJsonFromFile } from "@/utils/json";
 import { useUtilityStore } from "@/store/utility.store";
+import { saveJsonToFile, loadJsonFromFile } from "@/utils/json";
 import { saveImageToFile } from "@/utils/image";
 import { imageTypeSchema } from "@/schemas/asset.schema";
+import { ExportOptions } from "./export-options";
 
 export function FileMenu() {
   const importProject = useMainStore((s) => s.importProject);
   const exportProject = useMainStore((s) => s.exportProject);
   const canvas = useUtilityStore((s) => s.canvas);
   const onNextRenderFinished = useUtilityStore((s) => s.onNextRenderFinished);
+
+  const [exportOpen, setExportOpen] = useState(false);
 
   const exportImage = () => {
     canvas?.toBlob(async (blob) => {
@@ -22,34 +27,40 @@ export function FileMenu() {
         blob.type.split("/").at(-1) ?? "unknown",
       );
 
-      console.log(data);
       saveImageToFile("export", { data, type });
     });
   };
 
   return (
-    <Menu label="File" value="file">
-      <MenuItem
-        icon={<LuSave />}
-        onClick={() => saveJsonToFile(exportProject(), "project")}
-      >
-        Save
-      </MenuItem>
-      <MenuItem
-        icon={<LuFolderOpen />}
-        onClick={() => loadJsonFromFile(importProject)}
-      >
-        Load
-      </MenuItem>
+    <>
+      <Menu label="File" value="file">
+        <MenuItem
+          icon={<LuSave />}
+          onClick={() => saveJsonToFile(exportProject(), "project")}
+        >
+          Save
+        </MenuItem>
+        <MenuItem
+          icon={<LuFolderOpen />}
+          onClick={() => loadJsonFromFile(importProject)}
+        >
+          Load
+        </MenuItem>
 
-      <MenuSeparator />
+        <MenuSeparator />
 
-      <MenuItem
-        icon={<LuDownload />}
-        onClick={() => onNextRenderFinished(exportImage)}
-      >
-        Export image
-      </MenuItem>
-    </Menu>
+        <MenuItem
+          icon={<LuDownload />}
+          onClick={() => onNextRenderFinished(exportImage)}
+        >
+          Quick export as PNG
+        </MenuItem>
+
+        <MenuItem icon={<LuFileImage />} onClick={() => setExportOpen(true)}>
+          Export...
+        </MenuItem>
+      </Menu>
+      <ExportOptions open={exportOpen} onOpenChange={setExportOpen} />
+    </>
   );
 }
