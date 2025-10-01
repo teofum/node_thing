1fd08ed40dd6e9d3d6ec13c2b759784ad2c6c871
Sebalee <slee@itba.edu.ar>
2025-09-30 22:54:03 -0300

    chore: delete comments

--- src/app/components/menu/actions.ts
+++ src/app/components/menu/actions.ts
@@ -3,29 +3,6 @@
 import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 
-// export async function getUserData() {
-//   const supabase = await createClient();
-//   const {
-//     data: { user },
-//   } = await supabase.auth.getUser();
-
-//   if (!user) {
-//     return null;
-//   }
-
-//   const { data, error } = await supabase
-//     .from("profiles")
-//     .select("username, is_premium")
-//     .eq("id", user.id)
-//     .single();
-
-//   if (error) {
-//     throw new Error(`Failed to load user data: ${error.message}`);
-//   }
-
-//   return data ?? { is_premium: false };
-// }
-
 export async function saveProjectOnline(projectJSON: string) {
   const supabase = await createClient();
   const {
@@ -51,25 +28,3 @@ export async function saveProjectOnline(projectJSON: string) {
 
   return data;
 }
-
-// export async function getUserProjects() {
-//   const supabase = await createClient();
-//   const {
-//     data: { user },
-//   } = await supabase.auth.getUser();
-
-//   if (!user) {
-//     redirect("/auth/login?next=/profile");
-//   }
-
-//   const { data, error } = await supabase
-//     .from("projects")
-//     .select("id, data, created_at")
-//     .eq("user_id", user.id)
-
-//   if (error) {
-//     throw new Error(`Failed to load projects: ${error.message}`);
-//   }
-
-//   return data;
-// }
