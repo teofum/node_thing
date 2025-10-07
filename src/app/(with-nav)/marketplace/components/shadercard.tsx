b03b68522eadf3a93828abec5c9a4fd35cc586e0
Teo Fumagalli <teo.fum@gmail.com>
2025-10-07 09:10:30 -0300

    polish marketplace shader card a bit

--- src/app/(with-nav)/marketplace/components/shadercard.tsx
+++ src/app/(with-nav)/marketplace/components/shadercard.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
 import { LuCircleCheckBig, LuDownload, LuPlus } from "react-icons/lu";
-import { addToCart } from "../../marketplace/cart/actions";
+
 import { Button } from "@/ui/button";
+import { addToCart } from "../../marketplace/cart/actions";
 import { Stars } from "./stars";
 
 type ShaderCardProps = {
@@ -31,42 +33,45 @@ export default function ShaderCard({
   const isNew =
     Date.now() - new Date(createdAt).getTime() < 7 * 24 * 60 * 60 * 1000;
   return (
-    <div className="glass glass-border p-6 rounded-2xl relative">
+    <div className="glass glass-border p-4 rounded-2xl relative">
       {isNew && (
         <div className="absolute top-4 right-4 bg-red-800 text-white text-xs font-bold px-2 py-1 rounded-md shadow-lg">
           NEW
         </div>
       )}
-      <h3 className="text-xl font-semibold text-white mb-1">{title}</h3>
+      <div className="text-xl font-semibold text-white mb-1">{title}</div>
       {username && (
         <>
           <p className="text-sm text-white/60 mb-2">
             by <span className="font-bold">{username}</span>
           </p>
           {category && (
-            <p className="inline-block text-sm text-teal-400 border border-current/15 mb-4 font-semibold rounded-lg items-center justify-center gap-2  py-1 px-2">
+            <p className="inline-block text-sm text-teal-400 border border-current/15 font-semibold rounded-lg items-center justify-center gap-2  py-1 px-2">
               {category}
             </p>
           )}
         </>
       )}
-      <img
-        src="https://redthread.uoregon.edu/files/original/affd16fd5264cab9197da4cd1a996f820e601ee4.png"
-        alt="Shader preview"
-        className="w-full h-auto my-2 rounded-lg"
-      />
-      <h3 className="text-2xl font-bold text-teal-400">${price}</h3>
 
-      <div className="flex items-center justify-between mt-2">
-        <div className="flex flex-col gap-2">
-          <div className="flex items-center gap-2">
-            <LuDownload className="w-6 h-6" />
-            <span className="text-white text-base">{downloads}</span>
-          </div>
-          <Stars ratingValue={averageRating} ratingCount={ratingCount} />
+      <Image
+        src="/placeholder.webp"
+        width={1000}
+        height={667}
+        alt="Shader preview"
+        className="w-full aspect-[3/2] object-cover my-5 rounded-lg grayscale-100"
+      />
+
+      <div className="flex flex-row gap-3">
+        <div className="grow text-2xl font-bold text-teal-400">${price}</div>
+        <div className="flex flex-row items-center gap-1 text-white/60">
+          <LuDownload /> {downloads}
         </div>
+        <Stars ratingValue={averageRating} ratingCount={ratingCount} />
+      </div>
+
+      <div className="mt-2">
         {inCart ? (
-          <div className="flex items-center p-3 text-base/5 font-semibold text-white rounded-lg">
+          <div className="flex justify-center items-center h-13.5 text-base/5 font-semibold text-white rounded-lg border border-current/15 select-none">
             <LuCircleCheckBig className="inline mr-2 text-emerald-600" />
             In cart
           </div>
@@ -74,10 +79,9 @@ export default function ShaderCard({
           <form action={addToCart}>
             <input type="hidden" name="shaderId" value={id} />
             <Button
-              size="md"
+              size="lg"
               variant="outline"
-              className="flex items-center text-emerald-600 p-3"
-              icon
+              className="flex items-center text-emerald-600 w-full"
             >
               <LuPlus />
               Add to cart
