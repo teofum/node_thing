9daee0fc6d76d3f63517eee4264ea8bf361f38d1
Teo Fumagalli <teo.fum@gmail.com>
2025-10-14 01:39:55 -0300

    persist the animation store in localstorage

--- src/store/animation.store.ts
+++ src/store/animation.store.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { combine } from "zustand/middleware";
+import { combine, persist } from "zustand/middleware";
 
 export type AnimationState = "running" | "stopped" | "frame";
 type Animation = {
@@ -29,39 +29,43 @@ const initialState: Animation = {
 };
 
 export const useAnimationStore = create(
-  combine(initialState, (set) => ({
-    setState: (state: AnimationState) => set({ state }),
-    toggleState: () =>
-      set(({ state: oldState }) => ({
-        state: oldState === "stopped" ? "running" : "stopped",
-      })),
+  persist(
+    combine(initialState, (set) => ({
+      setState: (state: AnimationState) => set({ state }),
+      toggleState: () =>
+        set(({ state: oldState }) => ({
+          state: oldState === "stopped" ? "running" : "stopped",
+        })),
 
-    stop: () => set({ time: 0, frameIndex: 0, state: "stopped" }),
-    reset: () =>
-      set(({ state: oldState }) => ({
-        time: 0,
-        frameIndex: 0,
-        state: oldState === "running" ? "running" : "frame",
-      })),
+      stop: () => set({ time: 0, frameIndex: 0, state: "stopped" }),
+      reset: () =>
+        set(({ state: oldState }) => ({
+          time: 0,
+          frameIndex: 0,
+          state: oldState === "running" ? "running" : "frame",
+        })),
 
-    update: (frametime: number) =>
-      set(({ time, frameIndex, options }) => {
-        time = time + frametime;
-        frameIndex = frameIndex + 1;
+      update: (frametime: number) =>
+        set(({ time, frameIndex, options }) => {
+          time = time + frametime;
+          frameIndex = frameIndex + 1;
 
-        if (options.repeat && time > options.duration * 1000) {
-          time = 0;
-          frameIndex = 0;
-        }
+          if (options.repeat && time > options.duration * 1000) {
+            time = 0;
+            frameIndex = 0;
+          }
 
-        return { time, frameIndex };
-      }),
+          return { time, frameIndex };
+        }),
 
-    scrub: (time: number) => set({ time, frameIndex: 0, state: "frame" }),
+      scrub: (time: number) => set({ time, frameIndex: 0, state: "frame" }),
 
-    setOptions: (options: Partial<Animation["options"]>) =>
-      set(({ options: oldOptions }) => ({
-        options: { ...oldOptions, ...options },
-      })),
-  })),
+      setOptions: (options: Partial<Animation["options"]>) =>
+        set(({ options: oldOptions }) => ({
+          options: { ...oldOptions, ...options },
+        })),
+    })),
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    { name: "animation-store", partialize: ({ state, ...rest }) => rest },
+  ),
 );
