3b84caffba78a7c617aeab79cdafdb96419e9fa2
Santiago Lifischtz <santilifischtz@gmail.com>
2025-10-20 00:04:07 -0300

    DB functions for custom nodes

---
+++ src/app/components/workspace/actions.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+import { NodeType } from "@/schemas/node.schema";
+
+export async function saveShader(node: NodeType) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/auth/login");
+  }
+
+  const { error } = await supabase.from("shaders").insert({
+    user_id: user.id,
+    title: node.name.trim(),
+    description: null,
+    code: node.shader.trim(),
+    price: 0,
+    category_id: 0,
+    node_config: node,
+  });
+
+  if (error) {
+    throw new Error(`Failed to save shader: ${error.message}`);
+  }
+}
+
+export async function updateShader(node: NodeType) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/auth/login");
+  }
+
+  const { error } = await supabase.from("shaders").update({
+    title: node.name.trim(),
+    code: node.shader.trim(),
+    node_config: node,
+  });
+
+  if (error) {
+    throw new Error(`Failed to save shader: ${error.message}`);
+  }
+}
