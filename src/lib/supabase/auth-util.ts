8f3b62ea2484a110c710c06e9fba3ac5a1b2c26e
Sebalee <slee@itba.edu.ar>
2025-10-20 20:42:58 -0300

    feat: now with nextPath and  error handling

--- src/lib/supabase/auth-util.ts
+++ src/lib/supabase/auth-util.ts
@@ -1,14 +1,18 @@
 import { createClient } from "./server";
 import { redirect } from "next/navigation";
 
-export async function getSupabaseUserOrRedirect() {
+export async function getSupabaseUserOrRedirect(nextPath: string = "/") {
+  if (!nextPath.startsWith("/")) {
+    throw new Error(`Invalid nextPath: "${nextPath}", should start with '/'`);
+  }
+
   const supabase = await createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
   if (!user) {
-    redirect(`$/auth/login/?next=${encodeURIComponent("/")}`);
+    redirect(`/auth/login?next=${encodeURIComponent(nextPath)}`);
   }
 
   return { supabase, user };
