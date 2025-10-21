04daf9c26f1779a1f6b9d23cadac33cd0a7d37e7
Tiziano <tarcodaci@itba.edu.ar>
2025-10-21 20:59:27 -0300

    fix purchased star in custom shaders

--- src/app/components/workspace/shader-node/index.tsx
+++ src/app/components/workspace/shader-node/index.tsx
@@ -77,7 +77,8 @@ export function RenderShaderNode(
       >
         <div className="flex items-center gap-1">
           {nodeTypeInfo.name}
-          {nodeTypeInfo.externalShaderId ? (
+          {nodeTypeInfo.externalShaderId &&
+          nodeTypeInfo.category !== "Custom" ? (
             <LuStar className="w-3 h-3 opacity-70" />
           ) : null}
           <NodeMenu {...props} />
