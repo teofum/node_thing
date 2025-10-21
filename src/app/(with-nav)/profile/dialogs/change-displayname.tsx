253945b6bd07d398b684badce914bb145aea10f2
Sebalee <slee@itba.edu.ar>
2025-10-20 23:45:24 -0300

    feat: server side error handling

--- src/app/(with-nav)/profile/dialogs/change-displayname.tsx
+++ src/app/(with-nav)/profile/dialogs/change-displayname.tsx
@@ -16,60 +16,33 @@ export default function DisplayNameEditor({
   trigger,
   currentDisplayName,
 }: DisplayNameEditorProps) {
-  const displayNameRef = useRef<HTMLInputElement>(null);
-  const [isPending, setIsPending] = useState(false);
-  const [success, setSuccess] = useState(false);
-  const [error, setError] = useState<string | null>(null);
+  const [name, setName] = useState(currentDisplayName ?? "");
   const router = useRouter();
+
+  const displayNameChangeHandler = async () => {
+    await setDisplayName(name);
+    router.refresh();
+  };
 
   return (
     <Dialog trigger={trigger} title="Edit Display Name" description="">
       <div className="flex flex-col p-4 text-lg gap-4">
         <h1 className="mb-2">Display Name</h1>
-        <Input
-          ref={displayNameRef}
-          variant="outline"
-          size="md"
-          className="min-w-40"
-          defaultValue={currentDisplayName}
+        <textarea
+          //className="min-w-40"
+          className="text-sm resize-none max-w-full w-full outline-none p-2 rounded-lg border border-white/15"
           placeholder="Your display name"
+          value={name}
+          rows={1}
+          onChange={(e) => setName(e.target.value)}
         />
-        {error && <p className="text-sm text-red-400 mt-1">{error}</p>}
+
         <div className="flex justify-end gap-2">
-          <Button
-            onClick={async () => {
-              if (displayNameRef.current) {
-                const displayName = displayNameRef.current.value.trim();
-                setError(null);
-
-                if (displayName.length === 0) {
-                  setError("Display name cannot be empty");
-                  return;
-                }
-
-                setIsPending(true);
-                try {
-                  await setDisplayName(displayName);
-                  setIsPending(false);
-                  setSuccess(true);
-                  router.refresh();
-                  setTimeout(() => setSuccess(false), 2000);
-                } catch (err) {
-                  setError(
-                    err instanceof Error
-                      ? err.message
-                      : "Failed to update display name",
-                  );
-                  setIsPending(false);
-                }
-              }
-            }}
-            disabled={isPending || success}
-          >
-            {isPending ? "Saving..." : success ? "Saved!" : "Apply"}
-          </Button>
           <DialogClose asChild>
             <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <DialogClose asChild>
+            <Button onClick={() => displayNameChangeHandler()}>Save</Button>
           </DialogClose>
         </div>
       </div>
