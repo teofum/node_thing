4b4b8845d84b12629da96353e1ca6b81788d8cbe
lolo-pb <lpizzutobeltran@gmail.com>
2025-11-24 18:57:47 +0000

    base ui

---
+++ src/app/components/workspace/menu-history.tsx
@@ -0,0 +1,7 @@
+export function MenuHistory() {
+  return (
+    <div className="border-t border-white/15 flex flex-col h-full">
+      <div className="grow overflow-auto min-h-0 border-b border-white/15"></div>
+    </div>
+  );
+}
