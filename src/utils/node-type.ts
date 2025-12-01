7257aec2d3381c3d34f7ccc5bf9fcf85e00f168c
Santiago Lifischtz <santilifischtz@gmail.com>
2025-11-30 23:44:25 -0300

    added mirror and clamp

--- src/utils/node-type.ts
+++ src/utils/node-type.ts
@@ -875,7 +875,13 @@ export const NODE_TYPES = {
         type: "color",
       },
     },
-    parameters: {},
+    parameters: {
+      mode: {
+        name: "Wrapping mode",
+        type: "select",
+        options: ["Repeat", "Mirror", "Clamp"],
+      },
+    },
   },
   voronoi: {
     name: "Voronoi filter",
