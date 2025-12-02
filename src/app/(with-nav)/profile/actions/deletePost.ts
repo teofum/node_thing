3d3dc9f39eb13058045a5025110fe995d8387257
JuaniBridoux <jbridoux@itba.edu.ar>
2025-12-02 20:49:18 -0300

    Added redirect after delete

--- src/app/(with-nav)/profile/actions/deletePost.ts
+++ src/app/(with-nav)/profile/actions/deletePost.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+import { getUserData } from "./user";
 
 export async function deletePublication(id: string, type: string) {
   const supabase = await createClient();
@@ -10,4 +12,8 @@ export async function deletePublication(id: string, type: string) {
   const { error } = await supabase.from(table).delete().eq("id", id);
 
   if (error) throw error;
+
+  const { username } = await getUserData();
+
+  redirect(`/profile/${username}`);
 }
