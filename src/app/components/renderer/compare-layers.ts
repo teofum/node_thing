e01645ea63ffde4d93bf65b5338e8338438047b4
Teo Fumagalli <teo.fum@gmail.com>
2025-12-04 19:02:33 -0300

    fix layer comparison with groups

--- src/app/components/renderer/compare-layers.ts
+++ src/app/components/renderer/compare-layers.ts
@@ -1,17 +1,19 @@
 import { Edge } from "@xyflow/react";
 
 import { ShaderNode } from "@/schemas/node.schema";
-import { GroupNode, isGroup, Layer } from "@/store/project.types";
+import { Graph, GroupNode, isGroup, Layer } from "@/store/project.types";
 import { zip } from "@/utils/zip";
 
 function compareNodes(
   current: ShaderNode | GroupNode,
   last: ShaderNode | GroupNode,
+  displaySelection: boolean,
 ) {
   if (current.id !== last.id) return true;
 
-  // TODO compare groups
-  if (isGroup(current) || isGroup(last)) return true;
+  if (isGroup(current) && isGroup(last))
+    return compareGraphs(current.data, last.data, displaySelection);
+  if (isGroup(current) || isGroup(last)) return true; // Should be unreachable
 
   if (
     JSON.stringify(current.data.parameters) !==
@@ -34,9 +36,9 @@ function compareEdges(current: Edge, last: Edge, displaySelection: boolean) {
   return false;
 }
 
-export function compareLayers(
-  current: Layer,
-  last: Layer,
+export function compareGraphs(
+  current: Graph,
+  last: Graph,
   displaySelection: boolean,
 ) {
   /*
@@ -44,7 +46,7 @@ export function compareLayers(
    */
   if (current.nodes.length !== last.nodes.length) return true; // Nodes were added or removed
   for (const [currentNode, lastNode] of zip(current.nodes, last.nodes)) {
-    if (compareNodes(currentNode, lastNode)) return true; // A node is different
+    if (compareNodes(currentNode, lastNode, displaySelection)) return true; // A node is different
   }
 
   /*
