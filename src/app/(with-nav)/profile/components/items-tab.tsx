0a6492ccbae0cd0b8e603def42e7de2a8ec22fd2
JuaniBridoux <jbridoux@itba.edu.ar>
2025-12-08 18:34:58 -0300

    Solved issues

--- src/app/(with-nav)/profile/components/items-tab.tsx
+++ src/app/(with-nav)/profile/components/items-tab.tsx
@@ -116,8 +116,6 @@ const ItemsTab = forwardRef<HTMLDivElement, ItemTabsProps>(
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {projectsCards}
         </div>
-        <p className="text-md font-bold p-2 mt-5 mb-3">Groups</p>
-        TODO
       </div>
     );
   },
