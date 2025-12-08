1a8940f3f5a8a18ea8057e199ba22606e093152f
Teo Fumagalli <teo.fum@gmail.com>
2025-12-08 20:38:27 -0300

    device label for debugging

--- src/app/components/renderer/use-gpu.ts
+++ src/app/components/renderer/use-gpu.ts
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function useGPU() {
   const [device, setDevice] = useState<GPUDevice | null>(null);
+
+  const n = useRef(1);
 
   /*
    * Initialize GPU device on component init
@@ -13,10 +15,13 @@ export function useGPU() {
       const adapter = await navigator.gpu.requestAdapter();
       if (!adapter) throw new Error("couldn't get adapter");
 
-      const device = await adapter.requestDevice();
+      const device = await adapter.requestDevice({
+        label: `device ${n.current++}`,
+      });
+
       device.lost.then(({ reason }) => {
         // On device lost, reinitialize
-        console.warn(`WebGPU device lost: ${reason}`);
+        console.warn(`WebGPU device (${device.label}) lost: ${reason}`);
         if (reason !== "destroyed") initWebGPUDevice();
       });
 
